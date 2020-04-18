f58724befeccc5a1ab73e8ce1921c358b123cf1c
joshrenema2 <60946895+joshrenema2@users.noreply.github.com>
2020-04-18 16:47:09 -0700

    init commit

---
+++ comps/button-unselected/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./button-unselected.css";
+
+const ButtonUnselected = ({text,width, color, backgroundColor, onClick}) => <div 
+style={{width:width, color:color, backgroundColor:backgroundColor}}
+className="button_box"
+onClick={onClick}
+>
+    <div className="button_box_inner">
+        {text}
+    </div>
+</div>
+
+ButtonUnselected.defaultProps = {
+    text:"Button",
+    width:"150px",
+    color:"#000000",
+    backgroundColor:"#ededed",
+}
+
+export default ButtonUnselected;
---
+++ stories/test.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ButtonUnselected from "../comps/button-unselected";
+import ButtonSelected from "../comps/button-selected";
+
+export default {
+    title: "Pomān",
+};
+
+export const MyButtonUnselected = () => <ButtonUnselected />;
+export const MyButtonSelected = () => <ButtonSelected />;
