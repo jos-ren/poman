baa9a27ab35dafde9285a493f61c52c2e3113a52
joshrenema2 <60946895+joshrenema2@users.noreply.github.com>
2020-04-19 20:23:21 -0700

    added more circle buttons

--- comps/circle-button/index.js
+++ comps/circle-button/index.js
@@ -3,6 +3,11 @@ import "./circle-button.css";
 
 const nextW = require("../../img/1x/next-w.png");
 const next = require("../../img/1x/next.png");
+const spoonO = require("../../img/1x/spoon-o.png");
+const locateG = require("../../img/1x/locate-g.png");
+const messagesB = require("../../img/1x/messages-b.png");
+const edit = require("../../img/1x/edit.png");
+
 
 
 const CircleButton = ({ width, height, backgroundColor, img, filter }) => <div
@@ -14,9 +19,9 @@ const CircleButton = ({ width, height, backgroundColor, img, filter }) => <div
 CircleButton.defaultProps = {
     width: "50px",
     height: "50px",
-    backgroundColor: "#ededed",
-    img: next,
-    filter: "none",
+    backgroundColor: "#ffffff",
+    img: edit,
+    filter: "drop-shadow(0px 0px 2px rgba(0, 0, 0, 0.2))",
 }
 
 // CircleButton.defaultProps = {
--- comps/tab-bar/index.js
+++ comps/tab-bar/index.js
@@ -2,24 +2,31 @@ import React from "react";
 import "./tab-bar.css";
 
 const po = require("../../img/1x/po-g.png");
+const browse = require("../../img/1x/browse-o.png");
+const messages = require("../../img/1x/messages-b.png");
+const profile = require("../../img/1x/profile-p.png");
+const poUnselected = require("../../img/1x/po-l.png");
 const browseUnselected = require("../../img/1x/browse-l.png");
 const messagesUnselected = require("../../img/1x/messages-l.png");
 const profileUnselected = require("../../img/1x/profile-l.png");
 
-const TabBar = ({ width, backgroundColor, onClick }) =>
-    <div style={{ width: width, backgroundColor: backgroundColor }} className="tab_bar" onClick={onClick}>
+const TabBar = ({ width, backgroundColor, onClick, po, browse, messages, profile }) =>
+    <div style={{ width: width, backgroundColor: backgroundColor, po:po, browse:browse, messages: messages, profile:profile,  }} className="tab_bar" onClick={onClick}>
 
-        <div className="tab_bar_po tab_bar_inner"><img src={po} /></div>
-        <div className="tab_bar_browse tab_bar_inner"><img src={browseUnselected} /></div>
-        <div className="tab_bar_message tab_bar_inner"><img src={messagesUnselected} /></div>
-        <div className="tab_bar_profile tab_bar_inner"><img src={profileUnselected} /></div>
+        <div className="tab_bar_inner"><img src={po} /></div>
+        <div className="tab_bar_inner"><img src={browse} /></div>
+        <div className="tab_bar_inner"><img src={messages} /></div>
+        <div className="tab_bar_inner"><img src={profile} /></div>
 
     </div>
 
 TabBar.defaultProps = {
     width: "350px",
     backgroundColor: "#ffffff",
-    img: { po }
+    po:po,
+    browse:browseUnselected,
+    messages:messagesUnselected,
+    profile:profileUnselected,
 }
 
 export default TabBar;
